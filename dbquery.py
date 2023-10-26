a9603d2524968f8bdcd27a95fb421f05a0995a74
Oz Gitelson <47493278+OzGitelson@users.noreply.github.com>
2023-10-26 15:50:02 -0400

    create database

--- dbquery.py
+++ dbquery.py
@@ -6,33 +6,78 @@ def create_database():
     ex_statements=[]
 
     ex_statements.append('''
-    DROP TABLE colleges;
-    
+    DROP TABLE IF EXISTS colleges;
+    ''')
+
+    ex_statements.append('''
     CREATE TABLE colleges(
     id INT PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-    
-    
+    name TEXT NOT NULL
     );
     ''')
 
     ex_statements.append('''
-    DROP TABLE games;
-    
+    DROP TABLE IF EXISTS games;
+        ''')
+
+    ex_statements.append('''
     CREATE TABLE games(
     id INT PRIMARY KEY NOT NULL,
     location TEXT,
     time TEXT,
-    sport TEXT,
+    sport TEXT
     );
+        ''')
+
+    ex_statements.append('''
+    DROP TABLE IF EXISTS colleges_games;
     ''')
 
+    ex_statements.append('''
+    CREATE TABLE colleges_games(
+    c_id INT NOT NULL,
+    g_id INT NOT NULL,
+    winner INT DEFAULT FALSE,
+    score INT,
+    FOREIGN KEY(c_id) REFERENCES colleges(id),
+    FOREIGN KEY(g_id) REFERENCES  games(id),
+    PRIMARY KEY(c_id, g_id)
+    );
+        ''')
+
+    ex_statements.append('''
+    DROP TABLE IF EXISTS players;
+        ''')
+
+    ex_statements.append('''
+    CREATE TABLE players(
+        id INT PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL,
+        college INT NOT NULL,
+        FOREIGN KEY(college) REFERENCES colleges(id)
+    );
+        ''')
+
+    ex_statements.append('''
+    DROP TABLE IF EXISTS players_games;
+            ''')
+
+    ex_statements.append('''
+    CREATE TABLE players_games(
+        p_id INT NOT NULL,
+        g_id INT NOT NULL,
+        FOREIGN KEY(p_id) REFERENCES colleges(id),
+        FOREIGN KEY(g_id) REFERENCES  games(id),
+        PRIMARY KEY(p_id, g_id)
+    );
+            ''')
 
 
 
     with sql.connect("intramural.sqlite") as conn:
         with closing(conn.cursor()) as cursor:
-            pass
+            for ex_statement in ex_statements:
+                cursor.execute(ex_statement)
 
 
 create_database()
