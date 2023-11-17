8fc51b8afa2fec15195ac5fcae6453addd4aa43b
btd2026 <brian.dibassinga@gmail.com>
2023-11-17 16:22:47 -0500

    Search page Styling Update Mostly functioning

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify, make_response, redirect, session, current_app
 from werkzeug.exceptions import BadRequestKeyError
 from db_creator import get_teams
-from db_query import search_games, get_player_info
+from db_query import search_games, get_player_info, get_colleges
 from db_creator import sign_up_player
 from contextlib import closing
 import secrets
@@ -30,6 +30,8 @@ def main_page():
     search_terms = {}
     signed_in= "CAS_USERNAME" in session
     user=get_player_info(session['CAS_USERNAME'])[1] if signed_in else ''
+    # colleges = get_colleges()
+    # print(colleges)
 
     return render_template('index.html', search_terms=search_terms, signed_in=signed_in, username=user)
 
